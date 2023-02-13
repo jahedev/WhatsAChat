63d52acf1f87bdb287ebd56bd18141bbf2335839
MJ Hossain <jahedev@gmail.com>
2023-02-12 20:23:55 -0500

    theme context

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState } from "react"
+import { createContext } from "react"
+
+// type ThemeContextType = "light" | "dark"
+type Theme = "light" | "dark"
+
+interface ThemeContextType {
+  theme: Theme
+  toggleTheme?: () => void
+}
+
+const defaultTheme: ThemeContextType = { theme: "dark" }
+
+export const ThemeContext = createContext<ThemeContextType>(defaultTheme)
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [theme, setTheme] = useState(defaultTheme.theme)
+
+  const toggleTheme = () => {
+    setTheme(theme == "dark" ? "light" : "dark")
+  }
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      <div className="App" id="theme"></div>
+    </ThemeContext.Provider>
   )
 }
 
