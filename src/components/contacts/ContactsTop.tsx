256536981c57d41ad63dd2a7d4abf19c06aec8a9
MJ. Hossain <jahedev@gmail.com>
2023-02-20 16:45:23 -0500

    contacts top

--- src/components/contacts/ContactsTop.tsx
+++ src/components/contacts/ContactsTop.tsx
@@ -3,7 +3,9 @@ import React from 'react'
 export default function ContactsTopBar() {
   return (
     <div className='contacts-top'>
-      <div className='contacts-top__menu-button'>MENU</div>
+      <div className='contacts-top__menu-button'>
+        <span className='material-symbols-outlined'>menu</span>
+      </div>
       <div className='contacts-top__searchbar'>
         <input type='search' name='contacts-search' id='' />
       </div>
