6a0daf6084ee2381b47f87fb9379367238101d24
MJ. Hossain <jahedev@gmail.com>
2023-02-23 23:49:14 -0500

    content div

--- src/components/SideMenu.tsx
+++ src/components/SideMenu.tsx
@@ -1,13 +1,17 @@
 import React from 'react'
+import MaterialButton, { ButtonType as BT } from './common/MaterialButton'
 
 export default function SideMenu({ enabled }: { enabled: boolean }) {
   return (
-    <div className={`side-menu ${enabled ? 'side-menu__show' : ''}`}>
-      <div
-        className={`side-menu__side-bar ${
-          enabled ? 'side-menu__side-bar__show' : ''
-        }`}
-      ></div>
+    <div className={`sidemenu ${enabled ? 'sidemenu__show' : ''}`}>
+      <div className={`sidemenu__bar ${enabled ? 'sidemenu__bar__show' : ''}`}>
+        <div className='sidemenu__bar__content'>
+          <div className='sidemenu__bar__content__top'>
+            {/* <MaterialButton buttonType={BT.MENU} /> */}
+          </div>
+          <div className='sidemenu__bar__content__mid'>Hello</div>
+        </div>
+      </div>
     </div>
   )
 }
