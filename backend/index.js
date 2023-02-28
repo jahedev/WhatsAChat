83fae1c6bb4a162a54186a6b207982ecf8533251
MJ. Hossain <jahedev@gmail.com>
2023-02-28 12:34:13 -0500

    bl

--- backend/index.js
+++ backend/index.js
@@ -24,7 +24,7 @@ app.use(cors())
 app.use(express.json())
 
 app.get('/', (req, res) => {
-  res.send('Express + TypeScript Server')
+  res.send('Express + TypeScript  Server')
 })
 
 // ----- SIGNUP ROUTE -----
