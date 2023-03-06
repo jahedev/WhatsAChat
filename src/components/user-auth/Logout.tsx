2c593bf6027ed54977879550954b965935aa7b06
MJ. Hossain <jahedev@gmail.com>
2023-03-06 10:13:55 -0500

    logout redirects to login page

--- src/components/user-auth/Logout.tsx
+++ src/components/user-auth/Logout.tsx
@@ -1,6 +1,18 @@
-import React from 'react'
-// TODO: create logout functionality, redirect to Login page.
+import React, { useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
+
 export default function Logout() {
+  const navigate = useNavigate()
   localStorage.removeItem('token')
-  return <div></div>
+
+  useEffect(() => {
+    setTimeout(() => {
+      navigate('/')
+    }, 250)
+  }, [])
+  return (
+    <div>
+      <h1>You are being logged out...</h1>
+    </div>
+  )
 }
