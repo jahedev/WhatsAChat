21f4bd1d5afc7ad25535de7f94a08e5c09f56ae2
MJ. Hossain <jahedev@gmail.com>
2023-03-08 19:47:27 -0500

    separate auth route file

--- backend/index.js
+++ backend/index.js
@@ -4,8 +4,6 @@ const dotenv = require('dotenv')
 const cors = require('cors')
 const { MongoClient, ServerApiVersion } = require('mongodb')
 const mongoose = require('mongoose')
-const User = require('./models/user.model')
-const jwt = require('jsonwebtoken')
 
 // 21:56 - https://www.youtube.com/watch?v=Ejg7es3ba2k
 // ----- Configuration -----
@@ -27,72 +25,9 @@ app.get('/', (req, res) => {
   res.send('Express + TypeScript  Server')
 })
 
-// ----- SIGNUP ROUTE -----
-app.post('/api/register', async (req, res, next) => {
-  const { email, password, fullname } = req.body
-  const createdAt = Date()
-
-  if (!(await User.findOne({ email }))) {
-    await User.create({ email, password, fullname, createdAt })
-  } else {
-    return res.json({ status: 'error', message: 'Duplicate email address.' })
-  }
-
-  res.json({ status: 'ok', message: 'User created.' })
-  try {
-  } catch (error) {
-    res.json({ status: 'error', message: 'Internal Server Error.' })
-  }
-})
-
-// ----- LOGIN ROUTE -----
-app.post('/api/login', async (req, res, next) => {
-  const { email, password } = req.body
-
-  const user = await User.findOne({ email, password })
-
-  if (user) {
-    const token = jwt.sign(
-      {
-        email: user.email,
-      },
-      process.env.JWT_SECRET
-    )
-
-    return res.json({ status: 'ok', user: true, token })
-  } else {
-    return res.json({ status: 'ok', user: false })
-  }
-})
-
-// ----- QUOTE ROUTE -----
-app.get('/api/verifyAuth', async (req, res, next) => {
-  const token = req.headers['x-access-token']
-
-  try {
-    const decodedToken = jwt.verify(token, process.env.JWT_SECRET)
-    const { email } = decodedToken
-    const user = await User.findOne({ email })
-    if (user) {
-      return res.json({
-        status: 'ok',
-        user: true,
-        data: {
-          email: user.email,
-          fullname: user.fullname,
-        },
-      })
-    } else {
-      return res.json({ status: 'ok', user: false, token })
-    }
-  } catch (error) {
-    console.log(error)
-    res.json({ status: 'error', msg: 'Request was invalid.' })
-  }
-})
+// ----- ROUTES -----
+app.use('/api', require('./api'))
 
 app.listen(port, () => {
   console.log(`[server]: Server is running at http://localhost:${port}`)
 })
-
-// TODO: create middleware to verify login
---
+++ backend/routes/auth.route.js
@@ -0,0 +1,67 @@
+const router = require('express').Router()
+const User = require('./models/user.model')
+const jwt = require('jsonwebtoken')
+
+// ----- SIGNUP ROUTE -----
+app.post('/register', async (req, res, next) => {
+  const { email, password, fullname } = req.body
+  const createdAt = Date()
+
+  if (!(await User.findOne({ email }))) {
+    await User.create({ email, password, fullname, createdAt })
+  } else {
+    return res.json({ status: 'error', message: 'Duplicate email address.' })
+  }
+
+  res.json({ status: 'ok', message: 'User created.' })
+  try {
+  } catch (error) {
+    res.json({ status: 'error', message: 'Internal Server Error.' })
+  }
+})
+
+// ----- LOGIN ROUTE -----
+app.post('/login', async (req, res, next) => {
+  const { email, password } = req.body
+
+  const user = await User.findOne({ email, password })
+
+  if (user) {
+    const token = jwt.sign(
+      {
+        email: user.email,
+      },
+      process.env.JWT_SECRET
+    )
+
+    return res.json({ status: 'ok', user: true, token })
+  } else {
+    return res.json({ status: 'ok', user: false })
+  }
+})
+
+// ----- QUOTE ROUTE -----
+app.get('/verifyAuth', async (req, res, next) => {
+  const token = req.headers['x-access-token']
+
+  try {
+    const decodedToken = jwt.verify(token, process.env.JWT_SECRET)
+    const { email } = decodedToken
+    const user = await User.findOne({ email })
+    if (user) {
+      return res.json({
+        status: 'ok',
+        user: true,
+        data: {
+          email: user.email,
+          fullname: user.fullname,
+        },
+      })
+    } else {
+      return res.json({ status: 'ok', user: false, token })
+    }
+  } catch (error) {
+    console.log(error)
+    res.json({ status: 'error', msg: 'Request was invalid.' })
+  }
+})
